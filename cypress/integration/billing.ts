abb5cbbc3b1c849dc18de12c4a6361c9edeabbdb
Christopher Blanchard <cablanchard@gmail.com>
2020-05-27 16:56:36 +0200

    Pull in api-fixtures
    Test organisation, address lines and country field

--- cypress/integration/billing.ts
+++ cypress/integration/billing.ts
@@ -3,42 +3,41 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
 
+import { address as fixtures } from "@ideal-postcodes/api-fixtures";
+const address = fixtures.england;
+
 describe("Billing", () => {
-  let address: any;
   before(() => {
-    cy.visit('./checkout/billing.html', {
+    cy.visit("./checkout/billing.html", {
       onBeforeLoad: (window) => {
         // @ts-ignore
         window.idpcConfig = {
           apiKey: Cypress.env("API_KEY"),
           populateOrganisation: true,
-          autocompleteOverride: {}
+          autocompleteOverride: {},
         };
       },
       onLoad: (window) => {
-        const document = window.document
+        const document = window.document;
         const script = document.createElement("script");
         script.setAttribute("type", "text/javascript");
         script.setAttribute("src", "../dist/bigcommerce.min.js");
         document.body.appendChild(script);
-      }
+      },
     });
   });
 
-  beforeEach(function() {
-    cy.fixture("address.json").then(data => (address = data));
-  });
-
-  it("Autocomplete", function() {
+  it("Autocomplete", function () {
     cy.get("#checkoutBillingAddress").within(() => {
-      cy.get("#countryCodeInput").select("GB", { force: true });
-      cy.get("#addressLine1Input").clear({force: true}).type(address.street);
+      cy.get("#addressLine1Input").clear({ force: true }).type(address.line_1);
       cy.wait(5000);
-      cy.get("#addressLine1Input").clear({force: true}).type(address.street);
-      cy.get(".idpc_ul li")
-        .first()
-        .click();
-      cy.get("#cityInput").should("have.value", address.city);
+      cy.get("#addressLine1Input").clear({ force: true }).type(address.line_1);
+      cy.get(".idpc_ul li").first().click();
+      cy.get("#addressLine1Input").should("have.value", address.line_1);
+      cy.get("#addressLine2Input").should("have.value", address.line_2);
+      cy.get("#companyInput").should("have.value", address.organisation_name);
+      cy.get("#countryCodeInput").should("have.value", "GB");
+      cy.get("#cityInput").should("have.value", address.post_town);
       cy.get("#postCodeInput").should("have.value", address.postcode);
     });
   });
--- cypress/integration/shipping.ts
+++ cypress/integration/shipping.ts
@@ -1,39 +1,38 @@
+import { address as fixtures } from "@ideal-postcodes/api-fixtures";
+const address = fixtures.england;
+
 describe("Shipping", () => {
-  let address: any;
   before(() => {
-    cy.visit('./checkout/shipping.html', {
+    cy.visit("./checkout/shipping.html", {
       onBeforeLoad: (window) => {
         // @ts-ignore
         window.idpcConfig = {
           apiKey: Cypress.env("API_KEY"),
           populateOrganisation: true,
-          autocompleteOverride: {}
+          autocompleteOverride: {},
         };
       },
       onLoad: (window) => {
-        const document = window.document
+        const document = window.document;
         const script = document.createElement("script");
         script.setAttribute("type", "text/javascript");
         script.setAttribute("src", "../dist/bigcommerce.min.js");
         document.body.appendChild(script);
-      }
+      },
     });
   });
 
-  beforeEach(function() {
-    cy.fixture("address.json").then(data => (address = data));
-  });
-
-  it("Autocomplete", function() {
+  it("Autocomplete", function () {
     cy.get("#checkoutShippingAddress").within(() => {
-      cy.get("#countryCodeInput").select("GB", { force: true });
-      cy.get("#addressLine1Input").clear().type(address.street);
+      cy.get("#addressLine1Input").clear().type(address.line_1);
       cy.wait(5000);
-      cy.get("#addressLine1Input").clear().type(address.street);
-      cy.get(".idpc_ul li")
-        .first()
-        .click();
-      cy.get("#cityInput").should("have.value", address.city);
+      cy.get("#addressLine1Input").clear().type(address.line_1);
+      cy.get(".idpc_ul li").first().click();
+      cy.get("#addressLine1Input").should("have.value", address.line_1);
+      cy.get("#addressLine2Input").should("have.value", address.line_2);
+      cy.get("#companyInput").should("have.value", address.organisation_name);
+      cy.get("#cityInput").should("have.value", address.post_town);
+      cy.get("#countryCodeInput").should("have.value", "GB");
       cy.get("#postCodeInput").should("have.value", address.postcode);
     });
   });
